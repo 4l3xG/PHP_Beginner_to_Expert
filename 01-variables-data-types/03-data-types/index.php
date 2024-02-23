aadb588ca5d1ef7a9a1bf17149c993ebe750fe68
Alex Gillett <104741610+4l3xG@users.noreply.github.com>
2024-02-23 12:27:28 -0800

    Add files via upload
    https://github.com/4l3xG/PHP_Beginner_to_Expert

---
+++ 01-variables-data-types/03-data-types/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+// String
+
+$name = 'Alex Sever Gillett';
+$name2 = "Derya Sever Gillett";
+
+var_dump($name);
+echo '<br />';
+
+//Integer
+
+$age = 29;
+$age2 = 27;
+echo '<br />';
+
+//Float
+
+$rating = 4.5;
+var_dump($rating);
+
+//Array
+
+$friends = ['Bowie', 'Maya', 'Gabby'];
+
+var_dump($friends);
+
+
+//Object
+
+$person = new stdClass();
+var_dump($person);
